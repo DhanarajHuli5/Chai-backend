a51dcfd1c30369cce6561213120a5aad04eaa126
dhanaraj <dhanarajhuli5@gmail.com>
2024-08-03 22:40:27 +0530

    12 : routes and controllers

--- src/app.js
+++ src/app.js
@@ -1,16 +1,28 @@
 import express from "express";
-import cors from "cors"
+import cors from "cors";
 import cookieParser from "cookie-parser";
 
-const app = express()
+const app = express();
 
-app.use(cors({
+app.use(
+  cors({
     origin: process.env.CORS_ORIGIN,
-    credentials:true
-}))
+    credentials: true,
+  })
+);
 
-app.use(express.json({limit: "16kb"}))
-app.use(express.urlencoded({extended: true, limit: "16kb"}))
-app.use(express.static("public"))
-app.use(cookieParser())
-export { app }
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
+app.use(cookieParser());
+
+// routes import
+
+import userRouter from "./routes/user.routes.js";
+
+//routes declaration
+
+app.use("/users", userRouter);
+// http://localhost:8000/users/register
+
+export { app };
